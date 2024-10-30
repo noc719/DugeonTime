1fd8a3eb164faf0f04709102336d06664faba683
gwanyoungkim719 <0000kgy@naver.com>
2024-10-30 09:53:56 +0900

    [Add] ItemData

---
+++ Assets/Object/Item/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="ItemSlot",menuName ="ItemData",order = 0)]
+public class ItemData: ScriptableObject
+{
+    [Header("ItemInfo")]
+    public string itemName;
+    public string discription;
+
+    [Header("Cunsumable")]
+    public int value;
+}
---
+++ Assets/Object/Item/ItemSlot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData itemData;
+
+    public Image icon;
+}
